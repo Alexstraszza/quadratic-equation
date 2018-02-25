65d84d3b379c8fe117db57473e36c218a548de98
Alexstraszza <aostapenko66@gmail.com>
2018-02-25 13:16:30 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  equation = equation.replace(/\s/g, '');
+	var a = +equation.substr(0,equation.indexOf('*'));
+	equation = equation.slice(equation.indexOf('*')+4);
+	var b = +equation.substr(0,equation.indexOf('*'));
+	equation = equation.slice(equation.indexOf('*')+2);
+	var c = +equation;
+
+  var D=Math.pow(b,2)-4*a*c;
+    if (D>0) {
+      var x1=((-1)*b+Math.sqrt(D))/(2*a);
+      var x2=((-1)*b-Math.sqrt(D))/(2*a);
+    }
+    else {
+      var x1=(-1)*b+Math.sqrt(D)/(2*a);
+    }
+   if (x1>x2) { return ([Math.round(x2),Math.round(x1)]);
+        }
+        else {
+          return ([Math.round(x1),Math.round(x2)]);
+        }
+
+  }
